2d8844af8675c87a5e3a8b04189aeb196abc796d
Manal Mansoor. <manal.mansoor241@gmail.com>
2024-11-12 22:44:57 -0800

    Add files via upload

---
+++ ch 3 js/app.js
@@ -0,0 +1,25 @@
+//Chap 3
+
+
+//Question 1
+//let age = "I'm 20 years old.";
+//alert(age);
+
+
+
+//Question 2
+// let time = "You visited this website 10 times.";
+// alert(time);
+
+
+//Question 3
+// let birthyear = "My birth year is 2004.";
+// alert(birthyear);
+
+
+
+//Question 4
+// let visitorname = "Manal";
+// let saman = "10 Abayas";
+// let website = "Pk Abayas"
+// document.write(visitorname + "  ordered " + saman + " on " + website);
---
+++ ch 4 js/app.js
@@ -0,0 +1,31 @@
+// Chap 4
+
+
+
+//Question 1
+// let nuMber1 = "1",
+//     nuMber2 = "2",
+//     nuMber3 = "3";
+
+
+
+//Question 2
+// Illegal:
+//     "1number ";
+// "%number";
+// "let";
+// "var";
+// "const";
+
+// Legal:
+//     "Manal";
+// "maNal";
+// "_manal";
+// "&manal";
+// "manal";
+
+
+
+//Question 3
+// let fill = "Fill in the blanks.";
+// document.write(fill);
