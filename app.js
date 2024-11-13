5168fc46d917aa925ecd6551c42a1280f6332b5d
Manal Mansoor. <manal.mansoor241@gmail.com>
2024-11-12 22:51:52 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,51 @@
-//Chap 3
-
-
+// Chapter 2
 //Question 1
-//let age = "I'm 20 years old.";
-//alert(age);
-
+//let userName = "Manal";
+//  alert(userName);
 
 
 //Question 2
-// let time = "You visited this website 10 times.";
-// alert(time);
+// let myName = "Manal Mansoor";
+// console.log(myName)
 
 
 //Question 3
-// let birthyear = "My birth year is 2004.";
-// alert(birthyear);
-
+// let message = "Hello World!!";
+// alert(message);
 
 
 //Question 4
-// let visitorname = "Manal";
-// let saman = "10 Abayas";
-// let website = "Pk Abayas"
-// document.write(visitorname + "  ordered " + saman + " on " + website);
+// let Studentname = "Manal";
+// let Studentage = "20";
+// let Studentcourse = "Web & App Development";
+// alert("Student Name :  " + Studentname);
+// alert("Student Age :  " + Studentage);
+// alert("Student Course :  " + Studentcourse);
+
+
+//Question 5
+// let food = "Pizza";
+// let Pizza = food + "\n" + food.slice(0, 4) + "\n" + food.slice(0, 3) + "\n" + food.slice(0, 2) + "\n" + food.slice(0, 1);
+// alert(Pizza);
+
+
+
+//Question 6
+// let email = "example@example.com";
+// alert(email);
+
+
+//Question 7
+// let message = "A smarter way to learn JavaScript ";
+// alert(message);
+
+
+//Question 8
+// let message = "Yah!! I can write HTML content through JS.";
+// document.write(message);
+
+
+
+//Question 9
+// let message = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬ ";
+// alert(message);
